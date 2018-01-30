26a2922df0f34d366af0ed940a6919503c96f925
tshyeon <qvil1127@gmail.com>
2018-01-30 22:03:10 +0900

    Create Counter UI using state

--- redux-counter/src/components/Counter.js
+++ redux-counter/src/components/Counter.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
+  state = {
+    count: 0,
+  };
+
+  handleIncrement = () => {
+    this.setState({ count: this.state.count + 1 });
+  }
+  
+  handleDecrement = () => {
+    this.setState({ count: this.state.count - 1 });
+  }
+  
   render() {
+    const { count } = this.state;
+
     return (
       <div>
-        Counter
+        Count : {`${count} `}
+        <button onClick={this.handleIncrement}>
+          +
+        </button>
+        <button onClick={this.handleDecrement}>
+          -
+        </button>
       </div>
     );
   }
