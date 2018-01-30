7dd50493abb94296213c5ddce3d8f169ecff2455
tshyeon <qvil1127@gmail.com>
2018-01-30 22:33:49 +0900

    Apply redux and action, store test
    * Check your console log

--- redux-counter/src/components/Counter.js
+++ redux-counter/src/components/Counter.js
@@ -8,23 +8,19 @@ class Counter extends Component {
   handleIncrement = () => {
     this.setState({ count: this.state.count + 1 });
   }
-  
+
   handleDecrement = () => {
     this.setState({ count: this.state.count - 1 });
   }
-  
+
   render() {
     const { count } = this.state;
 
     return (
       <div>
         Count : {`${count} `}
-        <button onClick={this.handleIncrement}>
-          +
-        </button>
-        <button onClick={this.handleDecrement}>
-          -
-        </button>
+        <button onClick={this.handleIncrement}>+</button>
+        <button onClick={this.handleDecrement}>-</button>
       </div>
     );
   }
--- redux-counter/src/index.js
+++ redux-counter/src/index.js
@@ -2,6 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore } from 'redux';
+import reducer from './reducers';
+import { increment, decrement } from './actions';
+
+const store = createStore(reducer);
+/**
+ * [Stage 1] Log the initial state
+ */
+// console.log(store.getState());
+
+/**
+ * [Stage 2] Dispatch action then log to your console
+ */
+console.log("======== Start increment ========");
+store.dispatch(increment(1))
+console.log(store.getState());
+console.log("======== End increment ========");
+
+console.log("======== Start decrement ========");
+store.dispatch(decrement(1))
+console.log(store.getState());
+console.log("======== End decrement ========");
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
