f66281416d8b729ed35c8caa826792435b85f3f5
Haneul Lee <haneullee@Haneului-MacBook-Pro.local>
2018-02-07 22:11:45 +0900

    counter props, state fixed

--- helloworld/src/App.js
+++ helloworld/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import Content from './components/Content';
-import Content2 from './components/Content2';
+import Title from './components/Title';
 import Counter from './components/Counter';
 
 class App extends Component {
@@ -16,28 +15,21 @@ class App extends Component {
   }
 
   //컴포넌트가 dom위에 만들어지기 전 실행
-  componentWillMount() {
-  }
+  // componentWillMount() {
+  // }
 
   //컴포넌트 렌더링
   render() {
     return (
       <div className="App">
-        <Content title={this.props.title}/>
+        <Title title={this.props.title}/>
         <Counter number={this.state.count} onUpdate={this.updateCounter}/>
-        <Content2/>
-
-
-        
       </div>
     );
   }
 
-
   updateCounter(value) {
-    console.log(value);
     this.setState({count: value});
-    console.log(this.state.count);
   }
 
   /**
@@ -45,37 +37,35 @@ class App extends Component {
    * 보통 javascript 프레임웍 연동이나 setTimeout, ajax 처리 
    */
   
-  componentDidMount() {
+  // componentDidMount() {
     // var _this = this;
 
     // setTimeout(function() {
     //   _this.setState({ msg: "love" });
     // },3000)
-  }
+  // }
 
-  //컴포넌트가 prop을 새로 받았을 때 실행
-  componentWillReceiveProps() {
-  }
+  // //컴포넌트가 prop을 새로 받았을 때 실행
+  // componentWillReceiveProps() {
+  // }
 
-  //prop/state가 변경되었을 때, 리렌더링을 할지말지 정함
-  shouldComponentUpdate() {
-  }
+  // //prop/state가 변경되었을 때, 리렌더링을 할지말지 정함
+  // shouldComponentUpdate() {
+  // }
 
-  //컴포넌트 업데이트 전 실행
-  componentWillUpdate(){
-  }
+  // //컴포넌트 업데이트 전 실행
+  // componentWillUpdate(){
+  // }
 
-  //컴포넌트 리렌더링 후 실행
-  componentDidUpdate(){}
+  // //컴포넌트 리렌더링 후 실행
+  // componentDidUpdate(){}
 
-  //컴포넌트가 dom에서 사라진 후 실행
-  componentWillUnmount() {}
-
-
+  // //컴포넌트가 dom에서 사라진 후 실행
+  // componentWillUnmount() {}
 }
 
 App.defaultProps = {
-  title : 'This is the counter!'
+  title : 'This is a counter!'
 }
 
 export default App;
--- helloworld/src/components/Counter.js
+++ helloworld/src/components/Counter.js
@@ -1,38 +1,29 @@
 import React from 'react';
 
 class Counter extends React.Component {
-    updateValue(){
+
+    updateValue() {
+        /**
+         * 상위 App.js의 this.updateCounter를 props(onUpdate)로 넘겨 받음
+         * App.js의 this.state.count를 props(number)로 넘겨 받음
+         * this.updateCounter에서 setState를 통해 this.state.count값이 계속 변경됨
+         */
         this.props.onUpdate(this.props.number+1);
-        console.log(this.props.number);
     }
-    // handleClick() {
-    //     console.log(this.props.number);
-    //     this.setState({
-    //         test : this.state.test + 1
-    //     });
-    //     console.log(this.props.number);
-    // }
 
     constructor(props){
         super(props);
         this.state = {
-            test : this.props.number
+            test : 0
         };
+
         this.updateValue = this.updateValue.bind(this);
-        // this.handleClick = this.handleClick.bind(this);
-    }
-
-    componentWillReceiveProps(nextProps){
-        console.log(nextProps);
-        if (nextProps.number !== this.props.number) this.setState({ test: nextProps.number})
-
     }
 
     render(){
+        // 아래 this.props.number는 상위 App.js의 변경되는 this.state.count를 나타낸다.
         return (
             <div>
-                {/* <h1>{this.state.test}</h1> */}
-                {/* <button onClick={this.handleClick}>Count</button> */}
                 <h1>{this.props.number}</h1>
                 <button onClick={this.updateValue}>Count</button>
             </div>
