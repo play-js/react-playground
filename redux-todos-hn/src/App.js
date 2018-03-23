057809a4d7c5e5c8796224a012a5b2e8c8004534
tshyeon <qvil1127@gmail.com>
2018-03-23 23:03:09 +0900

    Fixed this.props.add
    * Point! Connect App  to TODO

--- redux-todos-hn/src/App.js
+++ redux-todos-hn/src/App.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
-import {connect} from 'react-redux';
-import { addTodo, removeTodo } from './actions/todo';
-import Title from './components/Title';
-import TODO from './components/TODO';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { addTodo, removeTodo } from "./actions/todo";
+import Title from "./components/Title";
+import TODO from "./components/TODO";
 
 class App extends Component {
   // add = () => {
@@ -13,10 +13,13 @@ class App extends Component {
   // }
 
   render() {
+    const { add } = this.props;
+    // console.log(add);
+
     return (
       <div className="App">
-        <Title title={this.props.title}/>
-        <TODO/>
+        <Title title={this.props.title} />
+        <TODO add={add} /> // App의 add를 TODO의 add로 연결해줘야함
       </div>
     );
   }
@@ -26,21 +29,21 @@ function mapStateToProps(state) {
   return {
     // count: state.count
     id: 0,
-    text: 'Javascript',
+    text: "Javascript",
     completed: false
   };
 }
 
 function mapDispatchToProps(dispatch) {
   return {
-    add: () => dispatch(addTodo()), 
+    add: () => dispatch(addTodo()),
     remove: () => dispatch(removeTodo())
-  }
+  };
 }
 
 App.defaultProps = {
-  title : 'TODO LIST!'
-}
+  title: "TODO LIST!"
+};
 
 // export default connect(mapStateToProps)(App);
 export default connect(mapStateToProps, mapDispatchToProps)(App);
