507a68b2fd3817a91f4e9dea5789dbcd31151064
Taesu Hyeon <qvil1127@gmail.com>
2018-04-10 19:57:55 +0900

    Fixed error

--- shkyung_react/src/contactApp/ContactsApp.js
+++ shkyung_react/src/contactApp/ContactsApp.js
@@ -1,35 +1,40 @@
-import React, { Component } from 'react';
-import { render } from 'react-dom';
-import PropTypes from 'prop-types';
-import ContactList from './ContactList';
-import SearchBar from './SearchBar';
+import React, { Component } from "react";
+import { render } from "react-dom";
+import PropTypes from "prop-types";
+import ContactList from "./ContactList";
+import SearchBar from "./SearchBar";
 
 class ContactsApp extends Component {
-    constructor() {
-        super();
-        this.state = {
-            filterText: ""
-        }
-    }
+  constructor() {
+    super();
+    this.state = {
+      filterText: ""
+    };
+  }
 
-    handleUserInput(searchTerm) {
-          console.error("search bar on User input");
-        this.setState({ filterText: searchTerm })
-    }
+  handleUserInput(searchTerm) {
+    console.error("search bar on User input");
+    this.setState({ filterText: searchTerm });
+  }
 
-    render() {
-        return (
-            <div>
-                <SearchBar filterText={this.state.filterText} 
-                            onUserInput={this.handleUserInput.bind(this)} />
-                <ContactList contacts={this.props.contacts} filterText={this.state.filterText} />
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <SearchBar
+          filterText={this.state.filterText}
+          onUserInput={this.handleUserInput.bind(this)}
+        />
+        <ContactList
+          contacts={this.props.contacts}
+          filterText={this.state.filterText}
+        />
+      </div>
+    );
+  }
 }
 
 ContactsApp.propTypes = {
-    contacts: PropTypes.arrayOf(PropTypes.object)
-}
+  contacts: PropTypes.arrayOf(PropTypes.object)
+};
 
 export default ContactsApp;
